89786321946b65ac73fb3bb0a3f7ccec69bf1062
Rodion <fabiostare@yandex.ru>
2021-08-17 10:59:30 +0300

    refactor revertArray

--- revertArray.js
+++ revertArray.js
@@ -1,9 +1,3 @@
 const arr = [1, 2, 3, 4, 5];
 
-const revertArr = [];
-
-arr.forEach(value => {
-  revertArr.unshift(value);
-});
-
-console.log('revertArr:', revertArr);
+console.log('revertArr:', arr.reverse());
